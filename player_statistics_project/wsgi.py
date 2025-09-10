dfbc228702e65b0cb0113d7824ee8d02e69fe942
lutusyo <blutusyo@gmail.com>
2025-09-10 13:30:43 +0800

    delete all data

--- player_statistics_project/wsgi.py
+++ player_statistics_project/wsgi.py
@@ -1,8 +1,7 @@
-import sys
 import os
-
-sys.path.insert(0, '/home/afcportal/player_statistics2')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'player_statistics_project.settings'
-
 from django.core.wsgi import get_wsgi_application
+
+# Replace 'your_project' with your actual Django project folder name
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'player_statistics_project.settings')
+
 application = get_wsgi_application()
