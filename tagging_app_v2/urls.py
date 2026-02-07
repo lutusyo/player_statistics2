e689fd7a5650f287c4a91b100dd247e0d963c85a
Boniphace <blutusyo@gmail.com>
2026-02-07 08:42:58 +0300

    viewing pass data

--- tagging_app_v2/urls.py
+++ tagging_app_v2/urls.py
@@ -9,7 +9,7 @@ app_name = 'tagging_app_v2'
 
 urlpatterns = [
     path("match/<int:match_id>/tag_v2/", pass_network_enter_data.create_pass_event_v2, name="tag_panel_v2"),
-    path("match/<int:match_id>/pass-events/", pass_network.pass_events_v2_list, name="pass_events_v2_list"),
+    path("match/<int:match_id>/pass-events/", pass_network.pass_network_dashboard, name="pass_events_v2_list"),
     
 ]
 
