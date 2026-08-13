639bcbc0ccf51b894ada46fdf6adf32d4c8a41fa
lutusyo <blutusyo@gmail.com>
2026-08-13 14:38:42 +0300

    modify attachement delete html

--- apps/medical_data/views/attachment_views.py
+++ apps/medical_data/views/attachment_views.py
@@ -5,22 +5,7 @@ from apps.medical_data.models.medical_attachment import MedicalAttachment
 from django import forms
 from apps.medical_data.forms.medical_attachment_form import MedicalAttachmentForm
 
-class MedicalAttachmentForm(forms.ModelForm):
 
-    class Meta:
-        model = MedicalAttachment
-        fields = ["file","description",]
-
-        widgets = {
-            "file": forms.ClearableFileInput(
-                attrs={"class": "form-control",}
-            ),
-
-            "description": forms.TextInput(
-                attrs={"class": "form-control",
-                    "placeholder": "Example: MRI Right Knee",}
-            ),
-        }
 
 def medical_attachment_create(request, visit_id):
     medical_visit = get_object_or_404(MedicalVisit,pk=visit_id,)
@@ -46,15 +31,26 @@ def medical_attachment_create(request, visit_id):
     return render(request,"medical_data/medical_attachment/attachment_form.html", context)
 
 def medical_attachment_delete(request, pk):
-    attachment = get_object_or_404(MedicalAttachment,pk=pk,)
-    visit_id = attachment.visit.pk
+    attachment = get_object_or_404(MedicalAttachment, pk=pk)
+    medical_visit = attachment.visit
 
     if request.method == "POST":
         attachment.file.delete()
         attachment.delete()
 
-        messages.success(request,"Attachment deleted.")
-        return redirect("medical_data:medical_visit_detail",pk=visit_id,)
+        messages.success(request, "Attachment deleted.")
+        return redirect(
+            "medical_data:medical_visit_detail",
+            pk=medical_visit.pk
+        )
 
-    context = {"attachment": attachment,}
-    return render(request,"medical_data/medical_attachment/attachment_delete.html", context)
+    context = {
+        "attachment": attachment,
+        "medical_visit": medical_visit,
+    }
+
+    return render(
+        request,
+        "medical_data/medical_attachment/attachment_delete.html",
+        context
+    )
